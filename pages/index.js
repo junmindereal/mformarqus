87e60ea19b22446f70c161d0e6dd71780945411c
Junmin De Real <junmindereal@gmail.com>
2021-04-15 17:19:08 +0800

    contentful graphql configuration

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,36 @@
 import Head from 'next/head'
+import { GraphQLClient, gql } from 'graphql-request'
 
-export default function Home () {
+export async function getStaticProps () {
+  const endpoint = `https://graphql.contentful.com/content/v1/spaces/${process.env.SPACE_ID}`
+
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+    }
+  })
+
+  const postsQuery = gql`
+    {
+      blogPostCollection {
+        items {
+          title
+          slug
+        }
+      }
+    }
+  `
+
+  const posts = await graphQLClient.request(postsQuery)
+
+  return {
+    props: { posts }
+  }
+}
+
+export default function Home ({ posts }) {
+  console.log(posts)
+
   return (
     <>
       <Head>
